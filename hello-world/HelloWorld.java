5dfb6b5ecdc34acdbc0be74bf3065285bab82f4b
BuildTools <unconfigured@null.spigotmc.org>
2020-01-07 21:01:08 +0100

    La til mer dokumentasjon på HelloWorld.java

--- hello-world/HelloWorld.java
+++ hello-world/HelloWorld.java
@@ -1,6 +1,10 @@
+// "Hallo, Verden!" i Java
+
+// Oppretter public-klassen "HelloWorld". Noter at den er samme som filnavnet med "CamelCase".
 public class HelloWorld{
 
 public static void main(String[] args){
+	// Printer ut "Hallo, verden!" via System.out.print
 	System.out.print("Hallo, verden!");
 	}
 }
